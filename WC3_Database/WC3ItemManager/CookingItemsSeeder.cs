28b87d5cd1d17aa5d70da5877761071aed91ac43
ValdemarLabs <valtteha@gmail.com>
2026-07-29 12:28:00 +0300

    Updated CookingItemsSeeder so Cooking tier uses item_level_unclassified, while raw WC3 item level stays stack-cap based

--- WC3_Database/WC3ItemManager/CookingItemsSeeder.cs
+++ WC3_Database/WC3ItemManager/CookingItemsSeeder.cs
@@ -30,63 +30,63 @@ namespace WC3ItemManager
 
         private static readonly SeedItem[] Items =
         {
-            Food("j0c6", "Smoked Wolf Jerky", "Uncommon", 10, 65, "+1 Agility, +1 hit point regeneration."),
-            Food("j0c7", "Roasted Stag Haunch", "Uncommon", 10, 70, "+1 Strength, +50 maximum hit points."),
-            Food("j0c8", "Bear Fat Biscuit", "Uncommon", 12, 85, "+2 Strength, +100 maximum hit points, +1 hit point regeneration."),
-            Food("j0c9", "Boiled Makrura Claw", "Uncommon", 12, 90, "+2 Intelligence, +60 maximum mana, +1 mana regeneration."),
-            Food("j0d0", "Spiced Snake Strips", "Uncommon", 12, 75, "+2 Agility, +1 Critical Chance."),
-            Food("j0d1", "Fried Crawler Cake", "Uncommon", 12, 95, "+1 Armor, +1 Block, +1 hit point regeneration."),
+            Food("j0c6", "Smoked Wolf Jerky", "Uncommon", 1, 65, "+1 Agility, +1 hit point regeneration."),
+            Food("j0c7", "Roasted Stag Haunch", "Uncommon", 3, 70, "+1 Strength, +50 maximum hit points."),
+            Food("j0c8", "Bear Fat Biscuit", "Uncommon", 13, 85, "+2 Strength, +100 maximum hit points, +1 hit point regeneration."),
+            Food("j0c9", "Boiled Makrura Claw", "Uncommon", 18, 90, "+2 Intelligence, +60 maximum mana, +1 mana regeneration."),
+            Food("j0d0", "Spiced Snake Strips", "Uncommon", 8, 75, "+2 Agility, +1 Critical Chance."),
+            Food("j0d1", "Fried Crawler Cake", "Uncommon", 22, 95, "+1 Armor, +1 Block, +1 hit point regeneration."),
 
             FishFood("j2b0", "Brilliant Smallfish", "Common", 1, 18, "+1 hit point regeneration, +25 maximum hit points."),
-            FishFood("j2b1", "Slitherskin Mackerel", "Common", 15, 22, "+35 maximum mana, +1 mana regeneration."),
-            Food("j2a0", "Charred Boar Ribs", "Common", 75, 90, "+3 Strength, +3 Damage."),
-            FishFood("j2b2", "Mud Snapper Cake", "Common", 50, 60, "+75 maximum hit points, +1 Dodge."),
-            Stew("j2a1", "Rabbit Broth", "Common", 80, 85, "+2 hit point regeneration, +1 Dodge, +40 maximum mana."),
-            Food("j2a2", "Hawk Skewer", "Uncommon", 90, 105, "+3 Agility, +1 Hit, +4 Damage."),
-            FishFood("j2b3", "Rainbow Albacore", "Uncommon", 95, 110, "+2 Agility, +2 Hit, +1 mana regeneration."),
-            Stew("j2a3", "Turtle Stew", "Uncommon", 100, 125, "+2 Armor, +2 Block, +150 maximum hit points."),
-            Stew("j2b4", "Catfish Chowder", "Uncommon", 110, 135, "+125 maximum hit points, +2 hit point regeneration, +1 Armor."),
-            Stew("j2a4", "Murloc Fin Soup", "Uncommon", 120, 145, "+3 Intelligence, +90 maximum mana, +1 Spell Power."),
-            FishFood("j2b5", "Loch Frenzy Delight", "Uncommon", 125, 145, "+2 Critical Chance, +2 Hit, +4 Damage."),
-            Food("j2a5", "Lizard Pepper Roast", "Uncommon", 130, 150, "+3 Strength, +5 Damage, +1 Critical Chance."),
-            Stew("j2b6", "Firefin Chili", "Uncommon", 140, 175, "+5 Spell Power, +2 Critical Chance, -1 Armor."),
-            Food("j2a6", "Tiger Steak", "Uncommon", 150, 190, "+5 Agility, +2 Critical Chance, +7 Damage."),
-            Stew("j2b7", "Sagefish Soup", "Uncommon", 155, 200, "+4 Intelligence, +2 mana regeneration, +5 Spell Power."),
-            Food("j2a7", "Panther Fillet", "Uncommon", 165, 215, "+5 Agility, +2 Dodge, +12 movement speed."),
-            FishFood("j2b9", "Rockscale Cod", "Uncommon", 170, 225, "+3 Armor, +2 Block, +100 maximum hit points."),
-            Stew("j2a8", "Raptor Chili", "Rare", 180, 260, "+5 Strength, +8 Damage, +3 Critical Chance, -1 Hit."),
-            FishFood("j2c0", "Mithril Head Trout", "Uncommon", 190, 250, "+3 Block, +2 Armor, +3 Hit."),
-            Food("j2a9", "Cow Rump Roast", "Uncommon", 200, 280, "+4 Strength, +250 maximum hit points, +2 hit point regeneration."),
-            FishFood("j2c1", "Spotted Yellowtail", "Uncommon", 205, 275, "+4 Agility, +3 Hit, +10 movement speed."),
-            OddFood("j2d2", "Deviate Delight", "Rare", 215, 330, "+4 Agility, +3 Dodge, -2 Intelligence, +30 sight range."),
-            FishFood("j2c2", "Glossy Mightfish Steak", "Rare", 225, 390, "+7 Strength, +12 Damage, +4 Critical Chance."),
-            FishFood("j2c3", "Redgill Skillet", "Rare", 230, 405, "+4 Critical Chance, +5 Hit, +6 Damage."),
-            Stew("j2c4", "Nightfin Soup", "Rare", 240, 440, "+3 mana regeneration, +10 Spell Power, +180 maximum mana."),
-            FishFood("j2c5", "Sunscale Fillet", "Rare", 245, 450, "+4 hit point regeneration, +300 maximum hit points, +2 Armor."),
-            FishFood("j2c6", "Stonescale Eel", "Rare", 250, 475, "+4 Armor, +5 Block, -8 movement speed."),
-            FishFood("j2c7", "Whitescale Salmon", "Rare", 260, 520, "+5% Spell Power, +250 maximum mana, +4 Hit."),
-            Stew("j2c8", "Darkclaw Bisque", "Rare", 270, 600, "+8 Strength, +300 maximum hit points, +4 Armor."),
-            FishFood("j2c9", "Winter Squid", "Rare", 280, 650, "+8 Agility, +5 Critical Chance, +6 Dodge."),
-            FishFood("j2d0", "Summer Bass", "Rare", 290, 675, "+20 movement speed, +5 Hit, +5 Dodge."),
-            OddFood("j2d1", "Tigerseye Eel", "Rare", 300, 720, "+25 Spell Power, +5 Dodge, +5% Spell Power."),
-            OddFood("j2d3", "Ember Whelp Roast", "Rare", 285, 700, "+20 Damage, +5% Spell Power, +5 Critical Chance, -2 Armor."),
-            OddFood("j2d4", "Plaguebloom Dumpling", "Rare", 295, 730, "+8 Critical Chance, +8 Spell Power, +40 sight range, -2 hit point regeneration."),
+            FishFood("j2b1", "Slitherskin Mackerel", "Common", 5, 22, "+35 maximum mana, +1 mana regeneration."),
+            Food("j2a0", "Charred Boar Ribs", "Common", 25, 90, "+3 Strength, +3 Damage."),
+            FishFood("j2b2", "Mud Snapper Cake", "Common", 17, 60, "+75 maximum hit points, +1 Dodge."),
+            Stew("j2a1", "Rabbit Broth", "Common", 27, 85, "+2 hit point regeneration, +1 Dodge, +40 maximum mana."),
+            Food("j2a2", "Hawk Skewer", "Uncommon", 30, 105, "+3 Agility, +1 Hit, +4 Damage."),
+            FishFood("j2b3", "Rainbow Albacore", "Uncommon", 32, 110, "+2 Agility, +2 Hit, +1 mana regeneration."),
+            Stew("j2a3", "Turtle Stew", "Uncommon", 33, 125, "+2 Armor, +2 Block, +150 maximum hit points."),
+            Stew("j2b4", "Catfish Chowder", "Uncommon", 37, 135, "+125 maximum hit points, +2 hit point regeneration, +1 Armor."),
+            Stew("j2a4", "Murloc Fin Soup", "Uncommon", 40, 145, "+3 Intelligence, +90 maximum mana, +1 Spell Power."),
+            FishFood("j2b5", "Loch Frenzy Delight", "Uncommon", 42, 145, "+2 Critical Chance, +2 Hit, +4 Damage."),
+            Food("j2a5", "Lizard Pepper Roast", "Uncommon", 43, 150, "+3 Strength, +5 Damage, +1 Critical Chance."),
+            Stew("j2b6", "Firefin Chili", "Uncommon", 47, 175, "+5 Spell Power, +2 Critical Chance, -1 Armor."),
+            Food("j2a6", "Tiger Steak", "Uncommon", 50, 190, "+5 Agility, +2 Critical Chance, +7 Damage."),
+            Stew("j2b7", "Sagefish Soup", "Uncommon", 52, 200, "+4 Intelligence, +2 mana regeneration, +5 Spell Power."),
+            Food("j2a7", "Panther Fillet", "Uncommon", 55, 215, "+5 Agility, +2 Dodge, +12 movement speed."),
+            FishFood("j2b9", "Rockscale Cod", "Uncommon", 57, 225, "+3 Armor, +2 Block, +100 maximum hit points."),
+            Stew("j2a8", "Raptor Chili", "Rare", 60, 260, "+5 Strength, +8 Damage, +3 Critical Chance, -1 Hit."),
+            FishFood("j2c0", "Mithril Head Trout", "Uncommon", 63, 250, "+3 Block, +2 Armor, +3 Hit."),
+            Food("j2a9", "Cow Rump Roast", "Uncommon", 67, 280, "+4 Strength, +250 maximum hit points, +2 hit point regeneration."),
+            FishFood("j2c1", "Spotted Yellowtail", "Uncommon", 68, 275, "+4 Agility, +3 Hit, +10 movement speed."),
+            OddFood("j2d2", "Deviate Delight", "Rare", 72, 330, "+4 Agility, +3 Dodge, -2 Intelligence, +30 sight range."),
+            FishFood("j2c2", "Glossy Mightfish Steak", "Rare", 75, 390, "+7 Strength, +12 Damage, +4 Critical Chance."),
+            FishFood("j2c3", "Redgill Skillet", "Rare", 77, 405, "+4 Critical Chance, +5 Hit, +6 Damage."),
+            Stew("j2c4", "Nightfin Soup", "Rare", 80, 440, "+3 mana regeneration, +10 Spell Power, +180 maximum mana."),
+            FishFood("j2c5", "Sunscale Fillet", "Rare", 82, 450, "+4 hit point regeneration, +300 maximum hit points, +2 Armor."),
+            FishFood("j2c6", "Stonescale Eel", "Rare", 83, 475, "+4 Armor, +5 Block, -8 movement speed."),
+            FishFood("j2c7", "Whitescale Salmon", "Rare", 87, 520, "+5% Spell Power, +250 maximum mana, +4 Hit."),
+            Stew("j2c8", "Darkclaw Bisque", "Rare", 90, 600, "+8 Strength, +300 maximum hit points, +4 Armor."),
+            FishFood("j2c9", "Winter Squid", "Rare", 93, 650, "+8 Agility, +5 Critical Chance, +6 Dodge."),
+            FishFood("j2d0", "Summer Bass", "Rare", 97, 675, "+20 movement speed, +5 Hit, +5 Dodge."),
+            OddFood("j2d1", "Tigerseye Eel", "Rare", 100, 720, "+25 Spell Power, +5 Dodge, +5% Spell Power."),
+            OddFood("j2d3", "Ember Whelp Roast", "Rare", 95, 700, "+20 Damage, +5% Spell Power, +5 Critical Chance, -2 Armor."),
+            OddFood("j2d4", "Plaguebloom Dumpling", "Rare", 98, 730, "+8 Critical Chance, +8 Spell Power, +40 sight range, -2 hit point regeneration."),
 
             Beverage("j3a0", "Springwater Tea", "Common", 1, 25, "+25 maximum mana, +1 mana regeneration."),
-            Beverage("j3a1", "Honeyed Milk", "Common", 20, 35, "+50 maximum hit points, -1 Intelligence."),
-            Beverage("j3a2", "Bitter Cactus Ale", "Common", 45, 55, "+1 Strength, -2 Intelligence, -1 Armor."),
-            Beverage("j3a3", "Stout Mead", "Uncommon", 75, 95, "+3 Strength, -2 Intelligence, -1 Armor."),
-            Beverage("j3a4", "Salted Makrura Broth", "Uncommon", 90, 110, "+1 mana regeneration, +60 maximum mana, -1 Armor."),
-            Beverage("j3a5", "Blackmouth Grog", "Uncommon", 120, 150, "+5 Damage, -2 Hit, -3 Intelligence."),
-            Beverage("j3a6", "Firefin Whiskey", "Uncommon", 150, 210, "+3 Critical Chance, +6 Damage, -4 Intelligence, -2 Armor."),
-            Beverage("j3a7", "Sagefish Tonic", "Uncommon", 175, 230, "+4 Intelligence, +1 mana regeneration, -1 Armor."),
-            Beverage("j3a8", "Deviate Rum", "Rare", 200, 310, "+5 Dodge, -5 Hit, +20 sight range, -4 Intelligence."),
-            Beverage("j3a9", "Nightfin Wine", "Rare", 225, 380, "+15 Spell Power, -20 movement speed, -2 Armor."),
-            Beverage("j3b0", "Stonescale Porter", "Rare", 250, 430, "+4 Armor, +4 Block, -4 Agility."),
-            Beverage("j3b1", "Lobster Bisque Cup", "Rare", 270, 500, "+200 maximum hit points, +2 hit point regeneration, -3 Intelligence."),
-            Beverage("j3b2", "Dragonfire Punch", "Rare", 285, 620, "+20 Damage, +5 Critical Chance, -5 Intelligence, -4 Armor."),
-            Beverage("j3b3", "Winter Squid Absinthe", "Rare", 295, 680, "+10% Spell Power, +10 Spell Power, -5 Hit, -6 Intelligence."),
-            Beverage("j3b4", "Brew of Bad Ideas", "Rare", 300, 750, "+10 Critical Chance, +10 Damage, -10 Intelligence, -5 Dodge, -5 Armor."),
+            Beverage("j3a1", "Honeyed Milk", "Common", 7, 35, "+50 maximum hit points, -1 Intelligence."),
+            Beverage("j3a2", "Bitter Cactus Ale", "Common", 15, 55, "+1 Strength, -2 Intelligence, -1 Armor."),
+            Beverage("j3a3", "Stout Mead", "Uncommon", 25, 95, "+3 Strength, -2 Intelligence, -1 Armor."),
+            Beverage("j3a4", "Salted Makrura Broth", "Uncommon", 30, 110, "+1 mana regeneration, +60 maximum mana, -1 Armor."),
+            Beverage("j3a5", "Blackmouth Grog", "Uncommon", 40, 150, "+5 Damage, -2 Hit, -3 Intelligence."),
+            Beverage("j3a6", "Firefin Whiskey", "Uncommon", 50, 210, "+3 Critical Chance, +6 Damage, -4 Intelligence, -2 Armor."),
+            Beverage("j3a7", "Sagefish Tonic", "Uncommon", 58, 230, "+4 Intelligence, +1 mana regeneration, -1 Armor."),
+            Beverage("j3a8", "Deviate Rum", "Rare", 67, 310, "+5 Dodge, -5 Hit, +20 sight range, -4 Intelligence."),
+            Beverage("j3a9", "Nightfin Wine", "Rare", 75, 380, "+15 Spell Power, -20 movement speed, -2 Armor."),
+            Beverage("j3b0", "Stonescale Porter", "Rare", 83, 430, "+4 Armor, +4 Block, -4 Agility."),
+            Beverage("j3b1", "Lobster Bisque Cup", "Rare", 90, 500, "+200 maximum hit points, +2 hit point regeneration, -3 Intelligence."),
+            Beverage("j3b2", "Dragonfire Punch", "Rare", 95, 620, "+20 Damage, +5 Critical Chance, -5 Intelligence, -4 Armor."),
+            Beverage("j3b3", "Winter Squid Absinthe", "Rare", 98, 680, "+10% Spell Power, +10 Spell Power, -5 Hit, -6 Intelligence."),
+            Beverage("j3b4", "Brew of Bad Ideas", "Rare", 100, 750, "+10 Critical Chance, +10 Damage, -10 Intelligence, -5 Dodge, -5 Armor."),
 
             Material("j4a0", "Coarse Flour", 5, 20, "Ground grain for biscuits, cakes, and dumplings."),
             Material("j4a1", "Honey", 5, 35, "Sweetener used in roasts, mead, and glazes."),
@@ -178,7 +178,7 @@ namespace WC3ItemManager
                 GoldCost = goldCost,
                 SellValue = System.Math.Max(1, goldCost / 5),
                 MaxCharges = 1,
-                MaxStack = 50,
+                MaxStack = 49,
                 IconPath = "ReplaceableTextures\\CommandButtons\\BTNTrueShot.blp",
                 Tooltip = name,
                 TooltipExtended = extended,
@@ -342,7 +342,7 @@ namespace WC3ItemManager
                 cmd.Parameters.AddWithValue("rarity_id", rarityId);
                 cmd.Parameters.AddWithValue("class_id", classId);
                 cmd.Parameters.AddWithValue("type_id", typeId);
-                cmd.Parameters.AddWithValue("item_level", item.Level);
+                cmd.Parameters.AddWithValue("item_level", GetWc3ItemLevel(item));
                 cmd.Parameters.AddWithValue("item_level_unclassified", item.Level);
                 cmd.Parameters.AddWithValue("required_level", 1);
                 cmd.Parameters.AddWithValue("gold_cost", item.GoldCost);
@@ -360,6 +360,21 @@ namespace WC3ItemManager
                 cmd.Parameters.AddWithValue("copy_base_abilities", item.CopyBaseAbilities);
                 cmd.ExecuteNonQuery();
             }
+        }
+
+        private static int GetWc3ItemLevel(SeedItem item)
+        {
+            if (item.ClassName == "Consumable" || item.ClassName == "Material")
+            {
+                if (item.MaxStack <= 0)
+                {
+                    return 0;
+                }
+
+                return System.Math.Min(item.MaxStack, 49);
+            }
+
+            return item.Level;
         }
 
         private static string GetColoredName(string name, string rarity)
