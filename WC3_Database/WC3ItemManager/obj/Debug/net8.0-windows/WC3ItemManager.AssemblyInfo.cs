a7fa8a55cb622dd84a57219f1632f40ad53c9b78
ValdemarLabs <valtteha@gmail.com>
2026-08-09 11:00:48 +0300

    New exports

--- WC3_Database/WC3ItemManager/obj/Debug/net8.0-windows/WC3ItemManager.AssemblyInfo.cs
+++ WC3_Database/WC3ItemManager/obj/Debug/net8.0-windows/WC3ItemManager.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © 2026")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Warcraft 3 Item Database Manager for Path of the Shaman")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbcffc0a55ea16faae0f52e6288f9bf535a7a12d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f30506c501fe491318862997e509ad3c6157c93")]
 [assembly: System.Reflection.AssemblyProductAttribute("WC3 Item Manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WC3ItemManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WC3_Database/WC3ItemManager/tmpobj/CodexDbTool/obj/Debug/net8.0/CodexDbTool.AssemblyInfo.cs
+++ WC3_Database/WC3ItemManager/tmpobj/CodexDbTool/obj/Debug/net8.0/CodexDbTool.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodexDbTool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c66084bf221e37c00036db202aee50c9d7ac1a89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f30506c501fe491318862997e509ad3c6157c93")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodexDbTool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodexDbTool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
